34dc361fd174f403fee7caa2fb9401a622eb5503
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2023-01-10 23:22:37 +0800

    Added HTML and CSS styling to the SMTP Message

--- src/utils/smtp.go
+++ src/utils/smtp.go
@@ -1,40 +1,75 @@
 package utils
 
 import (
+	"fmt"
 	"net/smtp"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func SMTP(username string, email string, otp string) {
 
-	// Loading the dotenv file
-	err := godotenv.Load("../../config/.env");
-	CheckError(err);
-
 	// Retrieve env variables
+	smtpAddress := os.Getenv("SMTP_ADDRESS")
 	smtpUsername := os.Getenv("SMTP_USERNAME")
 	smtpPassword := os.Getenv("SMTP_PASSWORD")
 	smtpHost := os.Getenv("SMTP_HOST")
-	smtpAddress := os.Getenv("SMTP_ADDRESS")
+
+    auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpHost)
 
     to := email
     subject := "IMS One-Time password"
-    body := "Dear " + username + ",\n\n" + "\tThis is an auto-generated email. Please do not reply to this email. \n\n\tThis is your OTP: " + 
-				otp + "\n\nRegards,\nThe IMS Team"
+    body := `
+        <html>
+            <head>
+                <style>
+                /* Add some styling for the email */
+                body {
+                    font-family: Arial, sans-serif;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    text-align: center;
+                }
+                h1 {
+                    margin-top: 20px;
+                    color: #333;
+                }
+                p {
+                    color: #555;
+                    text-align:left;
+                    padding-left:20px;
+                }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                <h1>IMS One-Time Password</h1>
+                <p>Hi <b>` + username + `</b>,</p>
+                <p>Your one-time password is:</p>
+                <h2>` + otp + `</h2>
+                <p>This password is valid for only 10 minutes. Please do not share this with anyone.</p>
+                <p>Thanks, <br>
+                    IMS Team
+                </p>
+                </div>
+            </body>
+        </html>
+    `
 
-    msg := "From: no-reply@IMS.com\n" + 
-            "To: " + to + "\n" + 
-            "Subject: " + subject + "\n\n" + 
-            body
+    // Create new message
+    message := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+    message += fmt.Sprintf("From: %s\r\n", "no-reply@IMS.com")
+    message += fmt.Sprintf("To: %s\r\n", email)
+    message += fmt.Sprintf("Subject: %s\r\n", subject)
+    message += fmt.Sprintf("\r\n%s\r\n", body)
 
-    err = smtp.SendMail(
+    err := smtp.SendMail(
         smtpAddress, // mailtrap.io SMTP server and port
-        smtp.PlainAuth("", smtpUsername, smtpPassword, smtpHost), // authentication
+        auth,
         "no-reply@IMS.com", // sender's email address
         []string{to}, // recipient's email address
-        []byte(msg), // email message
+        []byte(message), // email message
     )
 
     CheckError(err)
