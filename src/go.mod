15b8965f483d590add5b283cff243b4fade350d7
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2023-01-15 01:58:15 +0800

    Enabled CORS in backend

--- src/go.mod
+++ src/go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.7
+	github.com/rs/cors v1.8.3
 	golang.org/x/crypto v0.5.0
 )
--- src/main.go
+++ src/main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/LeonLow97/inventory-management-system-golang-react-postgresql/api/routes"
 	"github.com/LeonLow97/inventory-management-system-golang-react-postgresql/database"
 	"github.com/LeonLow97/inventory-management-system-golang-react-postgresql/keys"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -35,7 +37,23 @@ func main() {
 	// Connecting to localhost
 	port := fmt.Sprintf(":%v", os.Getenv("SERVER_PORT"))
 	fmt.Println("Server is running on port", port, "!")
-	err = http.ListenAndServe(port, mux)
+
+	// Enabling CORS for all routes
+	clientOrigin := os.Getenv("CLIENT_ORIGIN")
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{clientOrigin},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowedHeaders:   []string{"Content-Type"},
+		ExposedHeaders: []string{"Content-Length"},
+		AllowOriginFunc: func(origin string) bool {
+			return origin == clientOrigin
+		},
+		MaxAge: int(12 * time.Hour.Seconds()),
+	})
+	handler := c.Handler(mux)
+
+	err = http.ListenAndServe(port, handler)
 	if err != nil {
 		log.Println("Error in listening to port in main.go: ",err)
 	}
