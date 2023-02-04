98f9988ab8ae4a6ae8f249c8241ef40d3084525d
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2023-02-05 02:52:10 +0800

    Tested temporary home page on react

--- client/src/src/App.js
+++ client/src/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    background:
+      "linear-gradient(to right, #e84c3d, #f73859, #f73859, #f73859, #f73859, #e84c3d)",
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  text: {
+    fontSize: 16,
+    color: "#fff",
+    marginTop: 20,
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h1 style={styles.header}>
+        Welcome to Inventory Management System Home Page
+      </h1>
+      <p style={styles.text}>This is an application testing home page.</p>
     </div>
   );
 }
