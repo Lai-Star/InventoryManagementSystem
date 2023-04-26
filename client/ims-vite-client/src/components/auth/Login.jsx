24a0cce1064c0e32c34c28c95722d32fd1cca14e
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2023-04-27 00:56:51 +0800

    Created Login form and added CSS Styling

--- client/ims-vite-client/src/components/auth/Login.jsx
+++ client/ims-vite-client/src/components/auth/Login.jsx
@@ -1,6 +1,41 @@
+import { useState } from 'react'
+import './Login.css'
+
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log(email, password)
+    setEmail("")
+    setPassword("")
+  }
+
   return (
-    <></>
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2 className="login-title">Login</h2>
+        <div>
+          <label>Email</label>
+          <input value={email} onChange={handleEmailChange} autoFocus />
+        </div>
+        <div>
+          <label>Password</label>
+          <input value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 }
 
