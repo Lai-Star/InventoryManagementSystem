ef96076dde1589decd6df5e68bfce2f29e26c664
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2023-04-26 23:57:34 +0800

    React vite setup

---
+++ client/ims-vite-client/src/App.jsx
@@ -0,0 +1,7 @@
+import Login from "./components/auth/Login"
+
+function App() {
+  return <Login />
+}
+
+export default App
---
+++ client/ims-vite-client/src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el)
+
+root.render(<App />)
